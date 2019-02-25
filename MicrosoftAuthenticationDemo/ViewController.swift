d13ccdc27fc83b1a5eb9962725198cc5937dc44c
Simon Heij <sheij1@avans.nl>
2019-02-25 10:32:51 +0100

    Initial Commit

---
+++ MicrosoftAuthenticationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MicrosoftAuthenticationDemo
+//
+//  Created by simon heij on 25-02-19.
+//  Copyright © 2019 simon heij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
